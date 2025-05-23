a1bb1b349fd17222352c8867b1590e95b6c9de7c
이빛나 <dlql1306@naver.com>
2025-05-23 10:20:57 +0900

    저장
    저장

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerCondition condition;
+
+    private void Awake()
+    {
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
+    }
+}
+
---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+       
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (health.curValue < 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("플레이어가 죽었다.");
+    }
+}
