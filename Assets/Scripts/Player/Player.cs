247ed6a7dd98f6b06253f39eb838ed154e085f3a
이빛나 <dlql1306@naver.com>
2025-05-23 12:28:59 +0900

    저장1
    저장1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     public PlayerController controller;
     public PlayerCondition condition;
 
+    public ItemData itemData;
+    public Action addItem;
+
     private void Awake()
     {
         CharacterManager.Instance.Player = this;
